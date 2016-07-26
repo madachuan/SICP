1d781705aad92d1b3c429181a0f0fdeb0a9f11c7
madachuan <madachuanfanqiang@gmail.com>
2016-07-26 14:13:57 +0800

    Adjust the indentation of comments

--- eval-order-test.el
+++ eval-order-test.el
@@ -2,9 +2,8 @@
   "This function calls itself when invoked."
   (bootstrap))
 (defun eval-order-test (x y)
-  "Test whether the interpreter is
-using applicative-order evaluation
-or normal-order evaluation."
+  "Test whether the interpreter we are faced with is using
+ applicative-order evaluation or normal-order evaluation."
   (if (= x 0) 0 y))
 
 (eval-order-test 0 (bootstrap))
