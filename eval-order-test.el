466d4eeb61f08e9ee8e5c13e7e03daa627a5f722
madachuan <madachuanfanqiang@gmail.com>
2016-07-26 09:47:57 +0800

    Evaluation order test
    Test whether the interpreter is using applicative-order evaluation or normal-order evaluation.

---
+++ eval-order-test.el
@@ -0,0 +1,10 @@
+(defun bootstrap ()
+  "This function calls itself when invoked."
+  (bootstrap))
+(defun eval-order-test (x y)
+  "Test whether the interpreter is
+using applicative-order evaluation
+or normal-order evaluation."
+  (if (= x 0) 0 y))
+
+(eval-order-test 0 (bootstrap))
