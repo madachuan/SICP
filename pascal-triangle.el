7faf84f6bedab97949c1dae6d0334fb197ce8f53
madachuan <madachuanfanqiang@gmail.com>
2016-07-27 15:39:56 +0800

    Evaluate any element in a Pascal's triangle
        1
       1 1
      1 2 1
     1 3 3 1
    1 4 6 4 1
       ...

---
+++ pascal-triangle.el
@@ -0,0 +1,10 @@
+(defun pascal-triangle (row col)
+  "Evaluate any element based on the number of row 
+and the number of column in a Pascal's triangle."
+  (cond ((= row 0) 1)
+	((= col 0) 1)
+	((= row col) 1)
+	(t (+ (pascal-triangle (- row 1) col)
+	      (pascal-triangle (- row 1) (+ col 1))))))
+
+(pascal-triangle 4 2)
